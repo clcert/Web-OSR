6202c0ae13693c2492fda4f0fd10f581cf17fc44
Eduardo Acha <eduardoacha@gmail.com>
2015-12-06 22:28:42 -0300

    Added to the template the id of div tag

--- graphs/templatetags/ChartTemplate.py
+++ graphs/templatetags/ChartTemplate.py
@@ -4,8 +4,9 @@ register = template.Library()
 
 
 @register.inclusion_tag('charts/bars_chart.html')
-def bar_chart(bars):
-    return {'title': bars['title'],
+def bar_chart(id, bars):
+    return {'id': id,
+            'title': bars['title'],
             'title_x_axis': bars['xaxis'],
             'title_y_axis': bars['yaxis'],
             'x_label': bars['xvalues'],
