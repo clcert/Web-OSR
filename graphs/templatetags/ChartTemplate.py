baaaa18a50727e724325e69080ab9b13a6f1a3e6
Eduardo Acha <eduardoacha@gmail.com>
2015-12-06 22:19:13 -0300

    Added a template to plot a bars graph

---
+++ graphs/templatetags/ChartTemplate.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('charts/bars_chart.html')
+def bar_chart(bars):
+    return {'title': bars['title'],
+            'title_x_axis': bars['xaxis'],
+            'title_y_axis': bars['yaxis'],
+            'x_label': bars['xvalues'],
+            'values': bars['values']}
