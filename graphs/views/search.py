26a3f6105f286e11b9fafdf7e442b025e8bd0fb6
Eduardo Acha <eduardoacha@gmail.com>
2016-01-22 16:38:27 -0300

    Add search procedure to geo reference

--- graphs/views/search.py
+++ graphs/views/search.py
@@ -1,4 +1,5 @@
 import socket
+from geoip import geolite2
 
 from django.shortcuts import render
 
@@ -8,10 +9,14 @@ from graphs.models import Http80, Http443
 def search(request):
     ip = '192.80.24.6'
     reversed_dns = socket.gethostbyaddr(ip)
+    lat, long = geolite2.lookup(ip).location
+
     http80 = Http80.objects(ip=ip).order_by('-date').first()
     http443 = Http443.objects(ip=ip).order_by('-date').first()
     return render(request, 'graphs/search.html',
                   { 'ip': ip,
                     'reverse': reversed_dns[0],
+                    'lat': lat,
+                    'long': long,
                     'http80': http80,
                     'http443': http443})
