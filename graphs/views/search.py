9a914da0b4edae1c929ece66cd726c11bf05991a
Eduardo Acha <eduardoacha@gmail.com>
2016-01-23 17:32:50 -0300

    Add tables for http protocol in  port 8000 and 8080

--- graphs/views/search.py
+++ graphs/views/search.py
@@ -1,9 +1,7 @@
 import socket
 from geoip import geolite2
-
 from django.shortcuts import render
-
-from graphs.models import Http80, Http443
+from graphs.models import Http80, Http443, Http8080, Http8000
 
 
 def search(request):
@@ -13,10 +11,15 @@ def search(request):
 
     http80 = Http80.objects(ip=ip).order_by('-date').first()
     http443 = Http443.objects(ip=ip).order_by('-date').first()
+    http8000 = Http8000.objects(ip=ip).order_by('-date').first()
+    http8080 = Http8080.objects(ip=ip).order_by('-date').first()
+
     return render(request, 'graphs/search.html',
-                  { 'ip': ip,
-                    'reverse': reversed_dns[0],
-                    'lat': lat,
-                    'long': long,
-                    'http80': http80,
-                    'http443': http443})
+                  {'ip': ip,
+                   'reverse': reversed_dns[0],
+                   'lat': lat,
+                   'long': long,
+                   'http80': http80,
+                   'http443': http443,
+                   'http8000': http8000,
+                   'http8080': http8080})
