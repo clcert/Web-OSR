be77e893d26dc6c83deb4b1058f24a90478484cd
Eduardo Acha <eduardoacha@gmail.com>
2016-01-23 18:34:30 -0300

    Modify the html to use new template tag

--- graphs/views/search.py
+++ graphs/views/search.py
@@ -1,11 +1,12 @@
 import socket
 from geoip import geolite2
 from django.shortcuts import render
-from graphs.models import Http80, Http443, Http8080, Http8000
+from graphs.models import Http80, Http443, Http8080, Http8000, Https
 
 
 def search(request):
-    ip = '192.80.24.6'
+    # ip = '192.80.24.6'
+    ip = '201.220.232.16'
     reversed_dns = socket.gethostbyaddr(ip)
     lat, long = geolite2.lookup(ip).location
 
@@ -13,6 +14,8 @@ def search(request):
     http443 = Http443.objects(ip=ip).order_by('-date').first()
     http8000 = Http8000.objects(ip=ip).order_by('-date').first()
     http8080 = Http8080.objects(ip=ip).order_by('-date').first()
+
+    https = Https.objects(ip=ip).order_by('-date').first()
 
     return render(request, 'graphs/search.html',
                   {'ip': ip,
@@ -22,4 +25,5 @@ def search(request):
                    'http80': http80,
                    'http443': http443,
                    'http8000': http8000,
-                   'http8080': http8080})
+                   'http8080': http8080,
+                   'https': https})
