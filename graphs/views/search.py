9f60fb9d73a4d69868d81fee8aaf2098f734e639
twolfvb <tomas.wolf.vb@gmail.com>
2016-05-26 16:16:56 -0400

    search receives position

--- graphs/views/search.py
+++ graphs/views/search.py
@@ -6,6 +6,13 @@ from graphs.models import Http80, Http443, Http8080, Http8000, Https
 
 def search(request):
     ip = request.GET[u'question']
+    if u'position' not in request.GET:
+        date_position = 0
+        print "wat"
+    else:
+        date_position = int(request.GET[u'position'])
+        
+    # latest date is 0, increasing position goes back in time
     # ip = '201.220.232.16'
     try:
         reversed_dns = socket.gethostbyaddr(ip)
@@ -17,17 +24,33 @@ def search(request):
     except AttributeError:
         lat, long = None, None
 
-    http80 = Http80.objects(ip=ip).order_by('-date').first()
-    http443 = Http443.objects(ip=ip).order_by('-date').first()
-    http8000 = Http8000.objects(ip=ip).order_by('-date').first()
-    http8080 = Http8080.objects(ip=ip).order_by('-date').first()
-    https = Https.objects(ip=ip).order_by('-date').first()
+    http80 = Http80.objects(ip=ip).order_by('-date')[date_position]
+    try:
+        http443 = Http443.objects(ip=ip).order_by('-date')[date_position]
+    except IndexError:
+        http443 = None
+
+    try:
+        http8000 = Http8000.objects(ip=ip).order_by('-date')[date_position]
+    except IndexError:
+        http8000 = None
+
+    try:
+        http8080 = Http8080.objects(ip=ip).order_by('-date')[date_position]
+    except IndexError:
+        http8080 = None
+
+    try:
+        https = Https.objects(ip=ip).order_by('-date')[date_position]
+    except IndexError:
+        https = None
 
     return render(request, 'graphs/search.html',
                   {'ip': ip,
                    'reverse': reversed_dns[0],
                    'lat': lat,
                    'long': long,
+                   'datePosition': date_position,
                    'http80': http80,
                    'http443': http443,
                    'http8000': http8000,
