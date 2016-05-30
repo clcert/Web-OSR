d777a368f8adf725fc6ecc05ab52dbab5c7df2ca
Eduardo Acha <eduardoacha@gmail.com>
2016-05-30 12:37:24 -0400

    Added a schema for http in port 443, 8000, 8080

--- script/schema.sql
+++ script/schema.sql
@@ -7,13 +7,36 @@ CREATE TABLE IF NOT EXISTS zmap_log (
 	send_avg varchar(15) not null,
 	recv integer not null,
 	recv_avg varchar(15) not null,
-	hits varchar(15) not null
+	hits varchar(15) not null,
+	PRIMARY KEY (port, date)
 );
 
 CREATE TABLE IF NOT EXISTS http_port_80 (
 	ip inet not null,
 	date date not null,
-	data JSON not null
+	data JSON not null,
+  PRIMARY KEY (ip, date)
+);
+
+CREATE TABLE IF NOT EXISTS http_port_443 (
+	ip inet not null,
+	date date not null,
+	data JSON not null,
+  PRIMARY KEY (ip, date)
+);
+
+CREATE TABLE IF NOT EXISTS http_port_8000 (
+	ip inet not null,
+	date date not null,
+	data JSON not null,
+  PRIMARY KEY (ip, date)
+);
+
+CREATE TABLE IF NOT EXISTS http_port_8080 (
+	ip inet not null,
+	date date not null,
+	data JSON not null,
+  PRIMARY KEY (ip, date)
 );
 
 -- Permisos de la tabla
@@ -24,6 +47,18 @@ GRANT SELECT ON zmap_log TO web_osr;
 ALTER TABLE http_port_80 OWNER TO upload_data_osr;
 GRANT ALL ON http_port_80 TO upload_data_osr;
 GRANT SELECT ON http_port_80 TO web_osr;
+
+ALTER TABLE http_port_443 OWNER TO upload_data_osr;
+GRANT ALL ON http_port_443 TO upload_data_osr;
+GRANT SELECT ON http_port_443 TO web_osr;
+
+ALTER TABLE http_port_8000 OWNER TO upload_data_osr;
+GRANT ALL ON http_port_8000 TO upload_data_osr;
+GRANT SELECT ON http_port_8000 TO web_osr;
+
+ALTER TABLE http_port_8080 OWNER TO upload_data_osr;
+GRANT ALL ON http_port_8080 TO upload_data_osr;
+GRANT SELECT ON http_port_8080 TO web_osr;
 
 -- Funcion de particiones http port 80
 CREATE OR REPLACE FUNCTION http_port_80_partition_function() RETURNS TRIGGER
