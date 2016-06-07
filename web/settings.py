5b8192cd49e85c1470ff08112bb06cf2e15dca58
Eduardo Acha <eduardoacha@gmail.com>
2016-06-07 13:05:52 -0300

    Connect with the postgres database

--- web/settings.py
+++ web/settings.py
@@ -83,6 +83,14 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'data_osr',
+        'USER': 'web_osr',
+        'PASSWORD': 'hevia2016',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+    'users': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
