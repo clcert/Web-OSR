08f8a09afff8bc12b551efec301a7b3194d64969
Eduardo Acha <eduardoacha@gmail.com>
2016-06-09 16:14:46 -0300

    Added temporal tables for http

--- script/schema.sql
+++ script/schema.sql
@@ -41,6 +41,30 @@ CREATE TABLE IF NOT EXISTS http_port_8080 (
   PRIMARY KEY (ip, date)
 );
 
+-- Tablas Intermedias
+CREATE TABLE IF NOT EXISTS http_product (
+	port INTEGER NOT NULL,
+	date DATE NOT NULL,
+	product TEXT NOT NULL,
+	version TEXT,
+	total INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS http_os (
+	port INTEGER NOT NULL,
+	date DATE NOT NULL,
+	os TEXT NOT NULL,
+	version TEXT,
+	total INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS http_type (
+	port INTEGER NOT NULL,
+	date DATE NOT NULL,
+	type TEXT NOT NULL,
+	total INTEGER NOT NULL
+);
+
 -- Permisos de la tabla
 
 -- zmap_log
@@ -67,3 +91,18 @@ GRANT SELECT ON http_port_8000 TO web_osr;
 ALTER TABLE http_port_8080 OWNER TO upload_data_osr;
 GRANT ALL ON http_port_8080 TO upload_data_osr;
 GRANT SELECT ON http_port_8080 TO web_osr;
+
+-- http_product
+ALTER TABLE http_product OWNER TO upload_data_osr;
+GRANT ALL ON http_product TO upload_data_osr;
+GRANT SELECT ON http_product TO web_osr;
+
+-- http_os
+ALTER TABLE http_os OWNER TO upload_data_osr;
+GRANT ALL ON http_os TO upload_data_osr;
+GRANT SELECT ON http_os TO web_osr;
+
+-- http_type
+ALTER TABLE http_type OWNER TO upload_data_osr;
+GRANT ALL ON http_type TO upload_data_osr;
+GRANT SELECT ON http_type TO web_osr;
