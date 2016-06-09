0f313100a58fe9a3e13ce09a5f44d76b55bd0756
Eduardo Acha <eduardoacha@gmail.com>
2016-06-08 23:27:57 -0400

    Added temporal table to optimize database query

--- script/schema.sql
+++ script/schema.sql
@@ -41,7 +41,24 @@ CREATE TABLE IF NOT EXISTS http_port_8080 (
   PRIMARY KEY (ip, date)
 );
 
--- Permisos de la tabla
+-- Tablas Intermedias
+CREATE TABLE IF NOT EXISTS http_product (
+	port integer not null,
+	date date not null,
+  product text not null,
+  version text,
+  total integer not null
+);
+
+CREATE TABLE IF NOT EXISTS http_operating_system (
+	port integer not null,
+	date date not null,
+  operation_system text not null,
+  version text,
+  total integer not null
+);
+
+-- Permisos
 
 -- zmap_log
 ALTER TABLE zmap_log OWNER TO upload_data_osr;
@@ -67,3 +84,13 @@ GRANT SELECT ON http_port_8000 TO web_osr;
 ALTER TABLE http_port_8080 OWNER TO upload_data_osr;
 GRANT ALL ON http_port_8080 TO upload_data_osr;
 GRANT SELECT ON http_port_8080 TO web_osr;
+
+-- http_product
+ALTER TABLE http_product OWNER TO upload_data_osr;
+GRANT ALL ON http_product TO upload_data_osr;
+GRANT SELECT ON http_product TO web_osr;
+
+-- http_operating_system
+ALTER TABLE http_operating_system OWNER TO upload_data_osr;
+GRANT ALL ON http_operating_system TO upload_data_osr;
+GRANT SELECT ON http_operating_system TO web_osr;
