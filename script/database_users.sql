328340622854c90fd2ae51c55fa4fcdd92a1f696
Eduardo Acha <eduardoacha@gmail.com>
2016-06-14 12:24:25 -0300

    Moved the script o thesis script repository

--- script/database_users.sql
+++
@@ -1,6 +0,0 @@
--- Crear el usuario
-CREATE USER web_osr PASSWORD 'hevia2016';
-CREATE USER upload_data_osr PASSWORD 'uploadHevia2016';
-
--- Crear la base de datos
-CREATE DATABASE data_osr OWNER upload_data_osr;
--- script/query.sql
+++
@@ -1,2 +0,0 @@
-select 80 as port, date, data#>>'{metadata, service, product}' as product, data#>>'{metadata, service, version}' as version, count(*) as total from http_port_80 where data#>>'{metadata, service, product}'!='' group by product, version, port, date;
-
