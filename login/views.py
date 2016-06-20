cd5b70d303d0d45e3bd307c8c8600efafb679b9f
Eduardo Acha <eduardoacha@gmail.com>
2016-06-19 23:47:14 -0400

    Visual changes in the index

--- login/views.py
+++ login/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from graphs.models import ZmapLog
+
 
 def index(request):
-    return render(request, 'index.html')
+    http_scan_date = ZmapLog.objects.filter(port=80).last().date
+    https_scan_date = ZmapLog.objects.filter(port=443).last().date
+    scans = ZmapLog.objects.order_by('port', '-date').distinct('port')[:10]
+
+    return render(request, 'index.html',
+                  {'http_scan_date': http_scan_date,
+                   'https_scan_date': https_scan_date,
+                   'scans': scans})
